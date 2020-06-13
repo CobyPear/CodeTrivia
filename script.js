82af2dab83956a663dc4a57cff591227c109f134
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-06-12 19:07:53 -0500

    started psuedocode

---
+++ script.js
@@ -0,0 +1,72 @@
+/* code quiz
+    1) when the user visits the site, the page will display the title and a description as well as a highscore link on the top
+        as wel as a button to start the quiz
+        need a link to high score page on the top left and a timer on the top right
+    
+    2) when the user pushes the button, the page will populate with a question and the timer will begin
+        page has 4 answer buttons that appear to be ordered lists
+
+    3)correct answer stores points, wrong answer subtracts time from clock
+
+    4) when all questions are answered or timer == 0, game is over.
+
+    5) user can enter initials and save score for later viewing */
+
+
+
+
+
+
+// VARIABLES
+
+// variable to store high scores
+
+// variable to store user's name 
+
+// variable to store number of correct answers
+
+// variable to set initial timer
+var timer  = 75;
+
+
+
+
+// FUNCTIONS
+
+// question generator/ game start
+gamestart = () => {
+// timer
+    setInterval(() => {
+
+        
+    }, 1000 * timer);
+}
+
+
+
+
+
+
+
+//EVENT LISTENERS
+
+// on button click start quiz
+
+// on button click store answer/score
+
+// on cliick to show highscores
+
+// on click to clear high scores
+
+//on click to go back
+
+
+
+
+
+
+
+
+
+// ON LOAD
+
