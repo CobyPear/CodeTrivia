4d0c8af25a55e4ca57f6350c0e8ea9003445c616
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-06-14 12:20:28 -0500

    question now populates page on click of start game button

--- script.js
+++ script.js
@@ -3,10 +3,14 @@
         as wel as a button to start the quiz
         need a link to high score page on the top left and a timer on the top right
     
-    2) TODO: when the user pushes the button, the page will populate with a question and the timer will begin
+    2) when the user pushes the button, the page will populate with a question and the timer will begin
         page has 4 answer buttons that appear to be ordered lists
 
-    3)correct answer stores points, wrong answer subtracts time from clock
+    3) TODO: correct answer stores points, wrong answer subtracts time from clock
+        TODO: style list items so that they appear as buttons
+        add event listeners for the answer buttons
+            listeners will remove time if answer is wrong and store points in score variable if question is correct
+            both correct and wrong need to render to the page
 
     4) when all questions are answered or timer == 0, game is over.
 
@@ -34,11 +38,21 @@ var startButtonEl = document.getElementById('start-button');
 // variable for timer on page
 var timerEl = document.querySelector('.timer');
 
+// make an ordered list for questions to populate
+var aList = document.createElement('ol');
+
+// make the list items
+var li0 = document.createElement("li");
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
 // variable to store high scores
 
 // variable to store user's name 
+var hsInitial;
 
-// variable to store number of correct answers
+// variable to store number of correct answers aka user's score
+var score;
 
 // variable to set initial timer
 let secondsRemaning  = 75;
@@ -47,52 +61,57 @@ let secondsRemaning  = 75;
 var questions = [
     {   
         question: 'What do you need to make a string?',
-        answers: [
-            {text: 'quesiton marks', correct: false},
-            {text: 'quotation marks', correct: true},
-            {text: 'parenthesses', correct: false},
-            {text: 'script tag', correct: false}
-        ]
+        answers: {
+            a: 'question marks',
+            b: 'quotation marks',
+            c: 'parenthesses',
+            d: 'script tag'
+        },
+        correctAnswer: 'b'
     },
-    {
-        question: 'Question 2',
-        answers: [
-            {text: 'answer 1', correct: false},
-            {text: 'answer 2', correct: false},
-            {text: 'answer 3', correct: true},
-            {text: 'answer 4', correct: false}
-        ]
+    {   
+        question: 'Placeholder Question 2',
+        answers: {
+            a: 'Placeholder A 1',
+            b: 'Placeholder A 2',
+            c: 'Placeholder A 3',
+            d: 'Placeholder A 4'
+        },
+        correctAnswer: 'a'
     },
-    {
-        question: 'Question 3',
-        answers: [
-            {text: 'answer 1', correct: false},
-            {text: 'answer 2', correct: false},
-            {text: 'answer 3', correct: false},
-            {text: 'answer 4', correct: true}
-        ]
+    {   
+        question: 'Placeholder Question 3',
+        answers: {
+            a: 'Placeholder A 1',
+            b: 'Placeholder A 2',
+            c: 'Placeholder A 3',
+            d: 'Placeholder A 4'
+        },
+        correctAnswer: 'a'
     },
-    {
-        question: 'Question 4',
-        answers: [
-            {text: 'answer 1', correct: false},
-            {text: 'answer 2', correct: true},
-            {text: 'answer 3', correct: false},
-            {text: 'answer 4', correct: false}
-        ]
+    {   
+        question: 'Placeholder Question 4',
+        answers: {
+            a: 'Placeholder A 1',
+            b: 'Placeholder A 2',
+            c: 'Placeholder A 3',
+            d: 'Placeholder A 4'
+        },
+        correctAnswer: 'c'
     },
-    {
-        question: 'Question 5',
-        answers: [
-            {text: 'answer 1', correct: true},
-            {text: 'answer 2', correct: false},
-            {text: 'answer 3', correct: false},
-            {text: 'answer 4', correct: false}
-        ]
-    }
+    {   
+        question: 'Placeholder Question 5',
+        answers: {
+            a: 'Placeholder A 1',
+            b: 'Placeholder A 2',
+            c: 'Placeholder A 3',
+            d: 'Placeholder A 4'
+        },
+        correctAnswer: 'd'
+    },
 ];
 // current question array
-var currentQuestions;
+// var currentQuestions = [];
 
 
 
@@ -105,18 +124,20 @@ function timer() {
         secondsRemaning--;
         // render seconds remaing to page
         timerEl.textContent = 'Timer: ' + secondsRemaning;
-        console.log(secondsRemaning)
-        console.log('WORKING')
+        // console.log(secondsRemaning)
+        // console.log('WORKING')
         // if the user gets a question wrong, decrease the amount of seconds remaining by some amount
         // if (questions.answer == false) {
         //     // render 'Wrong' to the screen
         //     //decrease amount of seconds remaining
 
         //     //if secondsRemaining == 0, stop the game and clear the timer
-        if (secondsRemaning ==  0) {
+        if (secondsRemaning == 0) {
         //     clearInterval(timerInterval);
         clearInterval(timerInterval);
+        timerEl.textContent = 'Timer: ' + secondsRemaning;
         headerEl.innerHTML = 'Time is up!'
+        textEl.innerHTML = score;
         console.log("Time's up")
         }
     }, 1000);
@@ -124,6 +145,8 @@ function timer() {
 
 // question generator/ game start
 function gamestart() {
+    // hide start button
+    startButtonEl.classList.add('hide');
     //start timer
     timer();
 
@@ -132,7 +155,7 @@ function gamestart() {
     console.log('QUESTIONS: ', questions)
 
     // ask a question
-    // questionAsker(questions);
+    questionAsker();
 
     // // generate a question
     // questionAsker = (questions) => {
@@ -144,7 +167,31 @@ function gamestart() {
     // }
 };
 
+// populate the page with a question
+questionAsker = () => {
+    console.log('WORKING: ')
+    // append the list to be populated to the textEl
+    textEl.replaceWith(aList)
 
+    for (let i = 0; i < questions.length; i++) {
+        // render the question to the page
+        headerEl.innerHTML = questions[i].question;
+        // set each list item texto to the answer's text
+        li0.innerHTML = questions[i].answers.a;
+        li1.innerHTML = questions[i].answers.b;
+        li2.innerHTML = questions[i].answers.c;
+        li3.innerHTML = questions[i].answers.d;
+        //render each answer to the list
+        aList.appendChild(li0);
+        aList.appendChild(li1);
+        aList.appendChild(li2);
+        aList.appendChild(li3);
+    };
+    
+    
+    
+   
+}
 
 
 //EVENT LISTENERS
@@ -154,7 +201,7 @@ addEventListener('load', () => {
     // populate header
     headerEl.textContent = 'Welcome to the quiz'
     // populate the rules text
-    textEl.textContent = 'You have 75 seconds for the quiz. Each wrong answer will decrease your time by X. Correct answers will increase your score. At the end of the quiz, you can input your initials to save your highscore. Good luck!'
+    textEl.innerHTML = 'You have 75 seconds for the quiz. Each wrong answer will decrease your time by X. Correct answers will increase your score. At the end of the quiz, you can input your initials to save your highscore. Good luck!'
     // show the start button
     hideEl.classList.remove('hide');
 });
