cccdf58d6643946c81b2bd47a58603e5b33b422d
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-06-16 22:13:35 -0500

    centered text on buttons

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ var timerEl = document.querySelector('.timer');
 var aList = document.createElement('ol');
 // area to print result
 var result = document.getElementById('result')
-// make the answer buttons
+    // make the answer buttons
 var answerButton1 = document.createElement('button');
 var answerButton2 = document.createElement('button');
 var answerButton3 = document.createElement('button');
@@ -45,57 +45,86 @@ let secondsRemaning = 75;
 // timer variable
 var timerInterval;
 // questions
-var questions = [
-    {
+var questions = [{
         question: 'What do you need to make a string in JavaScript?',
         answers: {
-            a: 'a: question marks',
-            b: 'b: quotation marks',
-            c: 'c: parenthesses',
-            d: 'd: script tag'
+            a: '< >',
+            b: '" "',
+            c: 'c: ( )',
+            d: 'd: { }'
         },
         correctAnswer: 'b'
     },
     {
         question: 'Which of these is a complex data type in JavaScript?',
         answers: {
-            a: 'a: function',
-            b: 'b: boolean',
-            c: 'c: number',
-            d: 'd: string'
+            a: 'function',
+            b: 'boolean',
+            c: 'number',
+            d: 'string'
         },
         correctAnswer: 'a'
     },
     {
         question: 'Which of these can NOT be used to set a variable in JavaScript?',
         answers: {
-            a: 'a: var',
-            b: 'b: const',
-            c: 'c: is',
-            d: 'd: let'
+            a: 'var',
+            b: 'const',
+            c: 'is',
+            d: 'let'
         },
         correctAnswer: 'c'
     },
     {
         question: 'What character is used to select an id in CSS?',
         answers: {
-            a: 'a: .',
-            b: 'b: #',
-            c: 'c: !',
-            d: 'd: &'
+            a: '.',
+            b: '#',
+            c: '!',
+            d: '&'
         },
         correctAnswer: 'b'
     },
     {
-        question: 'The default display property of a <div> in HTML is...',
+        question: 'What are the three fundemental languages of the modern web?',
         answers: {
-            a: 'a: inline-block',
-            b: 'b: none',
-            c: 'c: inline',
-            d: 'd: block'
+            a: 'Java, Python, PHP',
+            b: 'Ruby, GO, Rust',
+            c: 'C, C#, C++',
+            d: 'HTML, CSS, JavaScript'
         },
         correctAnswer: 'd'
     },
+    {
+        question: 'What tool is best for debugging?',
+        answers: {
+            a: 'Array.map()',
+            b: 'window.close()',
+            c: 'console.log()',
+            d: 'hammer'
+        },
+        correctAnswer: 'c'
+    },
+    {
+        question: 'What does CSS stand for?',
+        answers: {
+            a: 'Cascading Style Sheets ',
+            b: 'Collating Syrup Sleds',
+            c: 'Canberra Secret Service',
+            d: 'Cresent Swiss Sammies'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: 'Name the responsive CSS famework developed at Twitter',
+        answers: {
+            a: 'Shoestrap ',
+            b: 'CSS For Less',
+            c: 'Bootstrap',
+            d: 'Nester'
+        },
+        correctAnswer: 'c'
+    }
 ];
 // current question array
 var currentQuestion;
@@ -117,7 +146,6 @@ function enterScore() {
     // print the score to the page
     divEl = document.createElement('div');
     scoreEl = document.createElement('score');
-    scoreEl.style.color = 'blueviolet';
     scoreEl.textContent = `Final Score: ${score}`;
     headerEl.appendChild(divEl);
     divEl.appendChild(scoreEl);
@@ -132,14 +160,14 @@ function enterScore() {
     form.appendChild(inputBox);
 
     // store the initials and go to high scores page
-    form.addEventListener('submit', function (e) {
+    form.addEventListener('submit', function(e) {
         e.preventDefault();
         // object that holds initials and scores
         var object = {
-            name: inputBox.value,
-            score: score
-        }
-        // push the object into the local storage
+                name: inputBox.value,
+                score: score
+            }
+            // push the object into the local storage
         scoreObjs.push(object)
         localStorage.setItem('scoreObj', JSON.stringify(scoreObjs));
         // open the scores.html page if initials are not blank
@@ -151,7 +179,7 @@ function enterScore() {
 
 // timer
 function timer() {
-    timerInterval = setInterval(function () {
+    timerInterval = setInterval(function() {
         // decrement seconds left
         secondsRemaning--;
         // render seconds remaing to page
@@ -183,7 +211,7 @@ function gamestart() {
     // shuffle the question order
     questions.sort(() => Math.random() - 0.5);
     console.log('QUESTIONS: ', questions)
-    // store the current question
+        // store the current question
     currentQuestion = questions[i];
 
     // ask a question
@@ -244,18 +272,25 @@ addEventListener('load', () => {
 
 // bind listener to class name for button nested in body
 // listen for click in body on class name
-document.addEventListener('click', function (e) {
+document.addEventListener('click', function(e) {
     // if the button clicked is an answer button,
     if (e.target.classList.contains('answer-button')) {
         console.log('answer buttons work');
         if (e.target.classList.contains(currentQuestion.correctAnswer)) {
             console.log('CORRECT ANSWER WORKING')
-            // add one to score
+                // add one to score
             score++;
+            if (result.classList.contains('wrong')) {
+                // clear class list from result
+                result.classList.remove('wrong');
+            };
+
+            // add 'correct' to class list to style lines RED
+            result.classList.add('correct');
             // render 'correct!' to screen
             result.textContent = 'Correct!'
-            // erase message after 1.5 seconds
-            setTimeout(function () {
+                // erase message after 1.5 seconds
+            setTimeout(function() {
                 result.textContent = ''
             }, 1000);
             // increment question index
@@ -264,12 +299,16 @@ document.addEventListener('click', function (e) {
             questionAsker();
         } else {
             console.log('WRONG ANSWER WORKING')
-            // for a wrong answer, take 10 seconds off the clock
+                // for a wrong answer, take 10 seconds off the clock
             secondsRemaning -= 10;
+            // clear class list from result
+            result.classList.remove('correct');
+            // add 'wrong' to class list to style lines RED
+            result.classList.add('wrong');
             // render 'Wrong!' to the page
             result.textContent = 'Wrong!'
-            // erase message after 1.5 seconds
-            setTimeout(function () {
+                // erase message after 1.5 seconds
+            setTimeout(function() {
                 result.textContent = ''
             }, 1000);
             //icrement question index
